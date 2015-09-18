649a90f9374c4e1dad25cf6e4ebe3620336336a7
Martin Salias <martin at salias dot com dot ar>
2015-09-18 16:37:45 -0300

    Put Echos between setup sections to ease troubleshooting

--- Vagrantfile
+++ Vagrantfile
@@ -69,28 +69,9 @@ Vagrant.configure(2) do |config|
 
     # Install GIT
     sudo apt-get -y install git
-    # Create CSD folder
-    cd ~
-    mkdir CSD
-    cd CSD
+
     # Clone Kleer Agile Development repo
+    git clone git@github.com:kleer-la/csd3_vm.git
 
-
-    # Install curl
-    sudo apt-get -y install curl
-
-    # Install RVM
-    gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-    \curl -sSL https://get.rvm.io | bash -s stable
-
-    # Install latest Ruby
-    source ~/.rvm/scripts/rvm
-    rvm install ruby
-
-    # Install AtomEditor
-    sudo apt-get -y install software-properties-common
-    sudo add-apt-repository -y ppa:webupd8team/atom
-    sudo apt-get -y update
-    sudo apt-get -y install atom
   SHELL
 end
--- setup.sh
+++ setup.sh
@@ -1,16 +1,29 @@
-# Install curl
+clear
+echo ========================================================================
+echo                  Kleer Agile Development virtual machine setup
+echo ========================================================================
+
+echo \n\n ==================================== Installing GIT ====================================
+sudo apt-get -y install git
+
+echo \n\n ==================================== Installing Curl ===================================
 sudo apt-get -y install curl
 
-# Install RVM
+echo \n\n ==================================== Installing RVM ====================================
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
 
-# Install latest Ruby
+echo \n\n ================================ Installing Latest Ruby ================================
 source ~/.rvm/scripts/rvm
 rvm install ruby
 
-# Install AtomEditor
+echo \n\n ================================ Installing Atom Editor ================================
 sudo apt-get -y install software-properties-common
 sudo add-apt-repository -y ppa:webupd8team/atom
 sudo apt-get -y update
 sudo apt-get -y install atom
+
+echo \n\n ================================ Creating CSD folders ================================
+cd ~
+mkdir CSD
+cd CSD
