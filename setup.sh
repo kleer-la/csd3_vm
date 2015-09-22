ff33fc8422db49fd73629bffcf3bc4f5bcdd26a2
Martin Salias <martin at salias dot com dot ar>
2015-09-22 17:06:09 -0300

    Add default JRE installation to setup script

--- setup.sh
+++ setup.sh
@@ -2,28 +2,36 @@ clear
 echo ========================================================================
 echo                  Kleer Agile Development virtual machine setup
 echo ========================================================================
-
-echo -e \n\n ==================================== Installing GIT ====================================
+echo
+echo ==================================== Installing GIT ====================================
 sudo apt-get -y install git
 
-echo -e \n\n ==================================== Installing Curl ===================================
+echo
+echo ==================================== Installing Curl ===================================
 sudo apt-get -y install curl
 
-echo -e \n\n ==================================== Installing RVM ====================================
+echo
+echo ==================================== Installing Java ===================================
+sudo apt-get -y install defaut-jre
+
+echo
+echo ==================================== Installing RVM ====================================
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
 
-echo -e \n\n ================================ Installing Latest Ruby ================================
+echo
+echo ================================ Installing Latest Ruby ================================
 source ~/.rvm/scripts/rvm
 rvm install ruby
 
-echo -e \n\n ================================ Installing Atom Editor ================================
+echo
+echo ================================ Installing Atom Editor ================================
 sudo apt-get -y install software-properties-common
 sudo add-apt-repository -y ppa:webupd8team/atom
 sudo apt-get -y update
 sudo apt-get -y install atom
 
-echo -e \n\n ================================ Installing Jenkins ================================
+echo -e \n\n ============================ Installing Jenkins ================================
 cd ~
 mkdir jenkins
 cd jenkins
@@ -42,7 +50,7 @@ java -jar jenkins-cli.jar -s ${targetUrl} restart
 java -jar jenkins-cli.jar -s ${targetUrl} shutdown
 cd ..
 
-echo -e \n\n ================================ Creating CSD folders ================================
+echo -e \n\n =========================== Creating CSD folders ================================
 cd ~
 mkdir CSD
 cd CSD
