23ead09b84ed340bc61dbfbd2d9cf6809fc71584
Marti Salias <martin@salias.com.ar>
2015-09-22 17:29:38 -0300

    Start Jenkins in the background to allow the plugins to be installed

--- setup.sh
+++ setup.sh
@@ -3,42 +3,43 @@ echo ========================================================================
 echo                  Kleer Agile Development virtual machine setup
 echo ========================================================================
 echo
-echo ==================================== Installing GIT ====================================
+echo ========================== Installing GIT =================================
 sudo apt-get -y install git
 
 echo
-echo ==================================== Installing Curl ===================================
+echo ========================== Installing Curl ================================
 sudo apt-get -y install curl
 
 echo
-echo ==================================== Installing Java ===================================
+echo ========================== Installing Java ================================
 sudo apt-get -y install defaut-jre
 
 echo
-echo ==================================== Installing RVM ====================================
+echo =========================== Installing RVM ================================
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
 
 echo
-echo ================================ Installing Latest Ruby ================================
+echo ======================== Installing Latest Ruby ===========================
 source ~/.rvm/scripts/rvm
 rvm install ruby
 
 echo
-echo ================================ Installing Atom Editor ================================
+echo ========================= Installing Atom Editor ==========================
 sudo apt-get -y install software-properties-common
 sudo add-apt-repository -y ppa:webupd8team/atom
 sudo apt-get -y update
 sudo apt-get -y install atom
 
-echo -e \n\n ============================ Installing Jenkins ================================
+echo
+echo ========================== Installing Jenkins =============================
 cd ~
 mkdir jenkins
 cd jenkins
 wget http://mirrors.jenkins-ci.org/war/latest/jenkins.war
 
 # Start
-java -jar jenkins.war
+java -jar jenkins.war &
 
 # Install plugins
 targetUrl=http://localhost:8080/
@@ -50,7 +51,8 @@ java -jar jenkins-cli.jar -s ${targetUrl} restart
 java -jar jenkins-cli.jar -s ${targetUrl} shutdown
 cd ..
 
-echo -e \n\n =========================== Creating CSD folders ================================
+echo
+echo ======================== Creating CSD folders =============================
 cd ~
 mkdir csd
 cd csd
